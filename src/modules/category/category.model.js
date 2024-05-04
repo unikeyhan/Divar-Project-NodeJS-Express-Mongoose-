7a7022988e2a2cd95011b0a8899f7aa1d82fbf1d
a4coder <eng.alirezashahryari@gmail.com>
2024-05-04 14:49:18 +0330

    add category model

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,22 @@
+const { Schema, Types, model } = require('mongoose');
+
+const CategorySchmea = new Schema(
+    {
+        name: { type: String, required: true },
+        slug: { type: String, required: true, index: true },
+        icon: { type: String, required: true },
+        parent: { type: Types.ObjectId, ref: 'Category', required: false },
+        parents: { type: [Types.ObjectId], ref: 'Category', required: false, default: [] },
+    },
+    { versionKey: false, id: false, toJSON: { virtuals: true } }
+);
+
+CategorySchmea.virtual('children', {
+    ref: 'Categury',
+    localField: '_id',
+    foreignField: 'parent',
+});
+
+const CategoryModel = model('categury', CategorySchmea);
+
+module.exports = CategoryModel;
