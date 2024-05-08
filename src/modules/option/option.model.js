047137010650f6653a5cf1af3ba759f9ca2396f4
a4coder <eng.alirezashahryari@gmail.com>
2024-05-08 19:42:07 +0330

    add Option schema to Option module

---
+++ src/modules/option/option.model.js
@@ -0,0 +1,12 @@
+const { Schema, Types } = require('mongoose');
+
+const OptionSchema = new Schema({
+    title: { type: String, required: true },
+    key: { type: String, required: true },
+    type: { type: String, enum: ['number', 'string', 'array', 'boolean'] },
+    enum: { type: Array, default: [] },
+    guide: { type: String, required: false },
+    category: { type: Types.ObjectId, ref: 'Category', required: true },
+});
+
+const OptionModel = Model('Option', OptionSchema);
