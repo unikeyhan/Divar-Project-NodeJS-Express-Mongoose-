dddd677b4a38a16e12c72eedeb22caea1aa17cee
a4coder <eng.alirezashahryari@gmail.com>
2024-05-08 20:16:45 +0330

    add option route to app.route

--- src/app.routes.js
+++ src/app.routes.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 const { AuthRouter } = require('./modules/auth/auth.routes');
 const { UserRouter } = require('./modules/users/user.routes');
 const { CategoryRouter } = require('./modules/category/category.routes');
+const { OptionRouter } = require('./modules/option/option.routes');
 
 const mainRouter = Router();
 mainRouter.use('/auth', AuthRouter);
 mainRouter.use('/user', UserRouter);
 mainRouter.use('/category', CategoryRouter);
+mainRouter.use('/option', OptionRouter);
 
 module.exports = mainRouter;
