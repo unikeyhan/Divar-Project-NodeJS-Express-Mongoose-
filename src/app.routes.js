69633b5f88ea1f64ecfda59e7c4ba47742ccf42f
Alireza Shahryari <eng.alirezashahryari@gmail.com>
2024-05-23 17:01:31 +0330

    complete post module

--- src/app.routes.js
+++ src/app.routes.js
@@ -3,14 +3,29 @@ const { AuthRouter } = require('./modules/auth/auth.routes');
 const { UserRouter } = require('./modules/users/user.routes');
 const { CategoryRouter } = require('./modules/category/category.routes');
 const { OptionRouter } = require('./modules/option/option.routes');
+const { PostRouter } = require('./modules/post/post.routes');
 
 const mainRouter = Router();
 mainRouter.use('/auth', AuthRouter);
 mainRouter.use('/user', UserRouter);
 mainRouter.use('/category', CategoryRouter);
 mainRouter.use('/option', OptionRouter);
-mainRouter.get('/', (req, res) => {
-    res.render('./pages/index.ejs');
-});
+mainRouter.use("/post", PostRouter);
+mainRouter.get("/", postController.postList);
+// mainRouter.get('/', (req, res) => {
+//     res.locals.layout = './layouts/website/main.ejs'
+//     res.render('./pages/home/index.ejs');
+// });
+// mainRouter.get("/:id", (req, res) => {
+//     res.locals.layout = "./layouts/website/main.ejs";
+//     res.render("./pages/home/index.ejs");
+// });
+// mainRouter.get("/panel", (req, res) => {
+//     res.render("./pages/panel/dashboard.ejs");
+// });
+// mainRouter.get("/auth/login", (req, res) => {
+//     res.locals.layout = "./layouts/auth/main.ejs";
+//     res.render("./pages/auth/login.ejs");
+// });
 
 module.exports = mainRouter;
--- src/modules/post/post.model.js
+++ src/modules/post/post.model.js
@@ -1,15 +1,22 @@
 const { Schema, Types, model } = require('mongoose');
 
-const PostSchema = new Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    category: { type: Types.ObjectId, ref: 'Category', required: true },
-    province: { type: String, required: true },
-    city: { type: String, required: true },
-    district: { type: String, required: true },
-    coordinate: { type: [Number], required: true },
-    images: { type: [String], required: false, default: [] },
-});
+const PostSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        userId: { type: Types.ObjectId, required: true },
+        amount: { type: Number, required: true, default: 0 },
+        content: { type: String, required: true },
+        category: { type: Types.ObjectId, ref: 'Category', required: true },
+        province: { type: String, required: true },
+        city: { type: String, required: true },
+        district: { type: String, required: true },
+        address: { type: String, required: false },
+        coordinate: { type: [Number], required: true },
+        images: { type: [String], required: false, default: [] },
+        options: { type: Object, default: {} },
+    },
+    { timeStamp: true }
+);
 
 const PostModel = model('Post', PostSchema);
 module.exports = PostModel;
